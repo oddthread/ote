a058c8adfc0fbe99f597157047babea5ea9ca9d5
oddthread <oddthread@gmail.com>
2017-08-13 11:31:40 -0500

    sync

--- src/h/action.h
+++ src/h/action.h
@@ -1,9 +1,17 @@
-/*This file was automatically generated.*/
 #ifndef d_action_h
 #define d_action_h
 
+#include "oul/src/h/oul.h"
+
+typedef struct action
+{
+    u32 uid;
+    vec2 cursor_position;
+    vec2 optional_end_position;
+	enum {ACTION_TEXT,ACTION_DELETE,ACTION_BACKSPACE} type;
+    char *text;
+} action;
+action *ctor_action(vec2 cursor_position, vec2 optional_end_position, char *text, u32 type);
 void dtor_action(action *a);
-action *ctor_action(vec2 cursor_position,vec2 optional_end_position,char *text,u32 type);
-extern u32 action_uid_counter;
 
 #endif
